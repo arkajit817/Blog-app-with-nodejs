9bc9b2a0b8aaf4e4a4046b1f4c4770abe9d83eec
Arkajit817 <arkajitchoudhury143@gmail.com>
2018-03-06 16:28:45 +0530

    changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,9 +8,9 @@ $('.live-search-box').on('keyup', function(){
 
 var searchTerm = $(this).val().toLowerCase();
 
-    $('.live-search-list li').each(function(){
+ $('.live-search-list li').each(function(){
 
-        if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+   if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
             $(this).show();
         } else {
             $(this).hide();
